9f40b7675a77eae781c2d9622228e10dc876dcf0
isebarn182@gmail.com <isebarn182@gmail.com>
2022-03-03 17:00:12 +0000

    first chart, timecard image chart

---
+++ pages/charts.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <div class="chart">
+      <LineChart :data="data" :options="options" :height="400" />
+    </div>
+  </section>
+</template>
+
+<script>
+import LineChart from '~/components/charts/LineChart.vue'
+export default {
+  components: { LineChart },
+
+  data () {
+    return {
+      data: [],
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    }
+  },
+
+  async fetch () {
+    const data = await this.$charts.$get('timecard_image_usage')
+    this.data = data
+  }
+}
+</script>
--- plugins/api.js
+++ plugins/api.js
@@ -15,4 +15,5 @@ export default function ({ $axios }, inject) {
   inject('api', axios($axios, process.env.AXIOS_API))
   inject('admin_operations', axios($axios, process.env.AXIOS_ADMIN_OPERATIONS))
   inject('csv_export', axios($axios, process.env.AXIOS_CSV_EXPORT))
+  inject('charts', axios($axios, process.env.AXIOS_CHARTS))
 }
